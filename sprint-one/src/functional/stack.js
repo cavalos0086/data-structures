5670cd3076c7cbd0fcd65c77ce9cf50c8613d59b
Cristian Avalos <cavalos0086@gmail.com>
2015-07-01 14:31:10 -0700

    Stack implemented

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,28 @@
 var Stack = function(){
   var someInstance = {};
 
+  // size of the stack instance:
+  var size = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value){
+    storage[size] = value;
+    size++;
   };
 
   someInstance.pop = function(){
+    if(size > 0){
+      var popElement =  storage[size-1];
+      delete storage[size];
+      size--;
+    }
+    return popElement;
   };
 
   someInstance.size = function(){
+    return size;
   };
 
   return someInstance;
