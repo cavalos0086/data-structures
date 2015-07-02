e37e3f3f6849217a6efd7e84074b11ec5ba248cc
cavalos0086 <cavalos0086@gmail.com>
2015-07-02 10:45:53 -0700

    Queue and Stack has been implemented.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeof = 0;
+  this.storage = {};
 };
+
+Queue.prototype.enqueue = function(value){
+	this.storage[this.sizeof] = value;
+	this.sizeof++;
+};
+Queue.prototype.dequeue = function(){
+	if(this.sizeof > 0){
+		var result = this.storage[0];
+		var tempObj = {};
+		for(var i = 1;i<this.sizeof;i++){
+			tempObj[i-1] =  this.storage[i];
+		}
+		this.storage = tempObj;
+		this.sizeof--;
+		return result;
+		
+	}
+};
+Queue.prototype.size = function(){
+	return this.sizeof;
+};
+
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,25 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.sizeof = 0;
+  this.storage = {};
+};
+
+Stack.prototype.pop = function(){
+	if(this.sizeof > 0){
+		var result = this.storage[this.sizeof-1];
+		delete this.storage[this.sizeof-1];
+
+		this.sizeof--;
+		return result;
+	}
+};
+Stack.prototype.size = function(){
+	return this.sizeof;
+};
+Stack.prototype.push = function(value){
+	this.storage[this.sizeof] = value;
+	this.sizeof++;
 };
 
 
