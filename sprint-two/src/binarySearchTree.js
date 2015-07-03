de568adc5348c30cc73ddfd10ccc5a749e8b2088
cavalos0086 <cavalos0086@gmail.com>
2015-07-03 15:57:22 -0700

    COMPLETED BINARYSEARCHTREE!!!!!!!!!!!!!!!!!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,4 +1,53 @@
 var BinarySearchTree = function(value){
+	this.value = value;
+	this.left = null;
+	this.right = null;
+};
+
+BinarySearchTree.prototype.insert = function(val){
+	if(val < this.value){
+		if(this.left === null){
+			this.left = new BinarySearchTree(val);
+		} else {
+			this.left.insert(val);
+		}
+	} else {
+		if(this.right === null){
+			this.right = new BinarySearchTree(val);
+		} else {
+			this.right.insert(val);
+		}
+	}
+};
+
+BinarySearchTree.prototype.contains = function(val){
+	if(this.value === val){
+		return true;
+	} else if(this.value > val && this.left !== null){
+		var truthy = this.left.contains(val);
+	} else if(this.value < val && this.right !== null){
+		var truthy = this.right.contains(val);
+	}
+
+	return !!(truthy);
+};
+
+BinarySearchTree.prototype.depthFirstLog = function(func){
+	// start at the root
+	// go left
+		// recursion root = this.left
+
+	// go right
+
+	func.call(this, this.value)
+
+	if(this.left !== null){
+		this.left.depthFirstLog(func);
+	}
+	if (this.right !== null){
+		this.right.depthFirstLog(func);
+	}
+
 };
 
 
