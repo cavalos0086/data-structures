b479551ba2c0fe05cbf2a506eb27f445792522a6
cavalos0086 <cavalos0086@gmail.com>
2015-07-02 17:34:27 -0700

    completed linked lists because WE ROOOOCCCCKKKKKKKK

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,43 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var temp = Node(value); //new node
+    temp.value = value; //set value
+
+
+    // if the list is not empty, set old tail.next to new tail(new item)
+    if(this.tail !== null){
+      this.tail.next = temp;
+    }
+
+
+    this.tail = temp; //sets tail as new tail
+
+    if(this.head === null){ //if there is no head (empty list) set head as new node
+       this.head = temp;
+    }
   };
 
   list.removeHead = function(){
+    var value = this.head.value; //stores value
+    var nextNode = this.head.next; // stores next
+    this.head = nextNode; //sets head to new head
+    return value; // returns old node value
   };
 
   list.contains = function(target){
+    var currentNode = this.head; // starts searching at head
+
+    do { // continues search until .next === null
+        console.log("node:", this.head); //log
+      if(currentNode.value === target){ // if current node === target
+        return true; // return true
+      } else {
+        currentNode = currentNode.next; // look at next node
+      }
+    } while(currentNode !== null)
+
+    return false // return false if not found
   };
 
   return list;
