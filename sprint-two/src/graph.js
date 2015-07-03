4e332f6447259712afd58ece70c8f77115b40e80
cavalos0086 <cavalos0086@gmail.com>
2015-07-03 12:12:32 -0700

    completed tree, backing up graph because we're probably about to break it real bad.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,21 +1,56 @@
 
 
 var Graph = function(){
+	this.nodeStorage = [];
 };
 
 Graph.prototype.addNode = function(node){
+	this.nodeStorage[this.nodeStorage.length] = GraphNode(node);
+
 };
 
 Graph.prototype.contains = function(node){
+	for(var i = 0;i<this.nodeStorage.length;i++){
+		if(this.nodeStorage[i].value === node){
+			return true;
+		}
+	}
+	return false;
 };
 
 Graph.prototype.removeNode = function(node){
+	//find node with value
+	//frmove node
+	for(var i=0;i<this.nodeStorage.length;i++){
+		if(this.nodeStorage[i].value === node){
+			this.nodeStorage.splice(i, 1);
+		}
+	}
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+	for(var i=0;i<this.nodeStorage.length;i++){
+		if(this.nodeStorage.edges.includes())
+	}
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+	var fromIndex;
+	var toIndex;
+
+	for(var i=0;i<this.nodeStorage.length;i++){
+		if(this.nodeStorage[i].value === fromNode){
+			fromIndex = i;
+		} else if (this.nodeStorage[i].value === toNode){
+			toIndex = i;
+		}
+	}
+	var fromEdges = this.nodeStorage[fromIndex].edges[];
+	var toEdges = this.nodeStorage[toIndex].edges[];
+
+	fromEdges[fromEdges.length] = this.nodeStorage[toIndex].value;
+	toEdges[toEdges.length] = this.nodeStorage[fromIndex].value;
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
@@ -28,5 +63,13 @@ Graph.prototype.forEachNode = function(cb){
  * Complexity: What is the time complexity of the above functions?
  */
 
+var GraphNode = function(value){
+  var node = {};
+
+  node.value = value;
+  node.edges = [];
+
+  return node;
+};
 
 
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value){
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -15,14 +15,28 @@ var Tree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+	var newTree = Tree(value);
+	this.children[this.children.length] = newTree;
+
 
 };
 
 treeMethods.contains = function(target){
+	if(this.value === target){
+		return true;
+	}
 
+	if(this.children.length > 0){
+		for(var i = 0;i<this.children.length; i++){
+			var truthy = this.children[i].contains(target);
+			if(truthy){return true;}
+		}
+	}
+	return false;
 };
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
