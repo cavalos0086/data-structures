698cd9a84c5480bb2e749cb0c0ad95e886b4fcb0
Cristian E. Avalos <cavalos0086@gmail.com>
2017-07-23 00:07:31 -0400

    finish linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,27 +1,111 @@
-var LinkedList = function(){
-  var list = {};
-  list.head = null;
-  list.tail = null;
+var LinkedList = function () {
+	var list = {};
+	list.head = null;
+	list.tail = null;
+	list.counter = 0;
 
-  list.addToTail = function(value){
-  };
+	list.addToTail = function (value) {
+		var newNode = Node(value);
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = this.tail.next;
+		}
+		this.counter++;
+	};
 
-  list.removeHead = function(){
-  };
+	list.addToHead = function(value) {
+		var newNode = Node(value);
+		if (this.head === null) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			newNode.next = this.head;
+			this.head = newNode;
+		}
+		this.counter++;
+	}
 
-  list.contains = function(target){
-  };
+	list.addAnywhere = function(value, position) {
+		var index = 0;
+		var ptr = this.head;
+		var newNode = Node(value);
 
-  return list;
+		while (index < position - 1 && !ptr) {
+			ptr = ptr.next;
+			index++;
+		}
+
+		temp = ptr.next;
+		ptr.next = newNode;
+		newNode.next = temp;
+		this.counter++;
+	}
+
+	list.removeHead = function () {
+		var temp = this.head.value;
+		this.head = this.head.next;
+		this.counter--;
+		return temp;
+	};
+
+	list.removeTail = function() {
+		var ptr = this.head;
+		var index = 0;
+
+		while (index < (this.counter - 2)) {
+			ptr = ptr.next;
+			index++;
+		}
+		this.tail = ptr;
+		this.counter--;
+	}
+	list.removeAt = function(position) {
+		var index = 0;
+		var ptr = this.head;
+
+		while (index < position - 1 && !ptr) {
+			ptr = ptr.next;
+			index++;
+		}
+
+		if (index === this.counter-1) {
+			this.removeTail();
+		} else {
+			ptr.next = ptr.next.next;
+		}
+
+		this.counter--;
+	}
+	list.contains = function (target) {
+		var index = 0;
+		var ptr = this.head;
+		var isInList = false;
+
+		while (index < this.counter) {
+			if (ptr.value === target) {
+				isInList = true;
+				break;
+			}
+			index++;
+			ptr = ptr.next;
+		}
+
+		return isInList;
+	};
+
+	return list;
 };
 
-var Node = function(value){
-  var node = {};
+var Node = function (value) {
+	var node = {};
 
-  node.value = value;
-  node.next = null;
+	node.value = value;
+	node.next = null;
 
-  return node;
+	return node;
 };
 
 /*
